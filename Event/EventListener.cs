92b233f565d5c97e42aef62464ef36b983d55d56
Poyraz <98493876+mh44a@users.noreply.github.com>
2023-10-06 21:15:39 +0300

    Create EventListener.cs

---
+++ Event/EventListener.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Event
+{
+    public interface EventListener
+    {
+      // TODO: Don't write anything in this class.
+    }
+}
